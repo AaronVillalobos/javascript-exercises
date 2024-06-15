04ed9fb56f8ec4b3aad02ac313888919f6ac6d59
AaronVillalobos <avilla4537@gmail.com>
2024-06-14 17:25:23 -0700

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, input) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] == input) {
+            arr.splice(i, 1);            
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
