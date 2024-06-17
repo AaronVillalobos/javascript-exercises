1e6c15cd8ad452d8b231872b7d247b86c455ff4f
AaronVillalobos <avilla4537@gmail.com>
2024-06-17 13:50:37 -0700

    Write sumAll function to return sum from range of given input numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    sum = 0;
 
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        for (let num = b; num <= a; num++) {
+            sum += (num);
+        }
+
+        return sum;
+    }
+    else {
+        for (let num = a; num <= b; num++) {
+            sum += num;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
